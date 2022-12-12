c7b3dee3598dbd0d6017880a04e4fd2933319e3e
Mikhail Chatillon <mikhail.chatillon@hotmail.com>
2022-12-12 21:23:26 +0100

    update naming

--- keyvault/main.tf
+++ keyvault/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_application_insights" "example" {
-  name                = "appins-${var.name}-${var.stage}"
+  name                = "kv-${var.name}-${var.stage}"
   location            = var.location
   resource_group_name = var.resource_group_name
   application_type    = "web"
